899ef3225124ef2806a15bba236bb59b25249b6a
vincent <vencnte@gmail.com>
2020-03-04 12:08:15 +0800

    add write block in chain

--- chain/src/store.rs
+++ chain/src/store.rs
@@ -1,6 +1,24 @@
+// Copyright 2019 MarcoPolo Protocol Authors.
+// This file is part of MarcoPolo Protocol.
+
+// MarcoPolo Protocol is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// MarcoPolo Protocol is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with MarcoPolo Protocol.  If not, see <http://www.gnu.org/licenses/>.
+
 use map_store::mapdb::MapDB;
 use map_store::config::Config;
 use map_store::Error;
+use core::block::Header;
+// use bincode;
 
 const HEADER_PREFIX: u8 = 'h' as u8;
 const HEAD_PREFIX: u8 = 'H' as u8;
@@ -12,10 +30,16 @@ pub struct ChainDB {
 }
 
 impl ChainDB {
+
     pub fn new() -> Result<Self, Error> {
         let cfg = Config::default();
         let mut m = MapDB::open(cfg).unwrap();
 
         Ok(ChainDB{db: m})
     }
+
+    // pub fn write_head(&self, h: &Header) -> Result<(), Error> {
+    //     let encoded: Vec<u8> = bincode::serialize(h).unwrap();
+    //     self.db.put(HEADER_PREFIX, encoded)
+    // }
 }
