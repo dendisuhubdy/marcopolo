ce22e4cf5c2873e95b8c496502176358cd050f6a
jasonberger11 <icefish0012@gmail.com>
2020-03-04 15:32:00 +0800

    signature.rs: update the struct of SignatureInfo

--- common/ed25519/src/signature.rs
+++ common/ed25519/src/signature.rs
@@ -26,47 +26,32 @@ use faster_hex::hex_string;
 use std::fmt::Error;
 use std::str::FromStr;
 
-
-#[derive(Clone)]
-pub struct SignatureInfo(pub [u8; SIGNATURE_LENGTH]);
-
-impl Default for SignatureInfo {
-    fn default() -> Self {
-        SignatureInfo([0u8;SIGNATURE_LENGTH])
-    }
-}
-impl fmt::Debug for SignatureInfo {
-    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        f.debug_struct("Signature")
-            .field("r", &hex_string(&self.0[0..32]).expect("hex string"))
-            .field("s", &hex_string(&self.0[32..64]).expect("hex string"))
-            .field("v", &hex_string(&self.0[64..65]).expect("hex string"))
-            .finish()
-    }
-}
+#[derive(Serialize, Deserialize)]
+#[derive(Debug, Default,Copy, Clone, Eq, Ord, PartialEq, PartialOrd)]
+pub struct SignatureInfo([u8; 32], [u8;32]);
 
 impl SignatureInfo {
-
-    /// Get a slice into the 'r' portion of the data.
     pub fn r(&self) -> &[u8] {
-        &self.0[0..32]
+        &self.0[..]
     }
-
-    /// Get a slice into the 's' portion of the data.
     pub fn s(&self) -> &[u8] {
-        &self.0[32..64]
+        &self.1[..]
     }
-
     pub fn to_signature(&self) -> Result<Signature, SignatureError> {
-        let data = &self.0;
-        let res = Signature::from_bytes(data);
+        let mut sig = [0u8; 64];
+        sig[0..32].copy_from_slice(&self.0[..]);
+        sig[32..64].copy_from_slice(&self.1[..]);
+        let res = Signature::from_bytes(&sig);
         res
     }
     pub fn from_signature(sign: &Signature) -> Self {
         let data = sign.to_bytes();
-        SignatureInfo(data)
+        let mut r = [0u8;32];
+        let mut s = [0u8;32];
+        r[..].copy_from_slice(&data[0..32]);
+        s[..].copy_from_slice(&data[32..64]);
+        SignatureInfo(r,s)
     }
-
     pub fn from_slice(data: &[u8]) -> Result<Self, SignatureError> {
         // let mut sig = [0u8; SIGNATURE_LENGTH];
         // sig[..].copy_from_slice(data);
@@ -74,3 +59,51 @@ impl SignatureInfo {
         Ok(SignatureInfo::from_signature(&sig))
     }
 }
+
+// #[derive(Clone)]
+// pub struct SignatureInfo(pub [u8; SIGNATURE_LENGTH]);
+
+// impl Default for SignatureInfo {
+//     fn default() -> Self {
+//         SignatureInfo([0u8;SIGNATURE_LENGTH])
+//     }
+// }
+// impl fmt::Debug for SignatureInfo {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+//         f.debug_struct("Signature")
+//             .field("r", &hex_string(&self.0[0..32]).expect("hex string"))
+//             .field("s", &hex_string(&self.0[32..64]).expect("hex string"))
+//             .field("v", &hex_string(&self.0[64..65]).expect("hex string"))
+//             .finish()
+//     }
+// }
+
+// impl SignatureInfo {
+
+//     /// Get a slice into the 'r' portion of the data.
+//     pub fn r(&self) -> &[u8] {
+//         &self.0[0..32]
+//     }
+
+//     /// Get a slice into the 's' portion of the data.
+//     pub fn s(&self) -> &[u8] {
+//         &self.0[32..64]
+//     }
+
+//     pub fn to_signature(&self) -> Result<Signature, SignatureError> {
+//         let data = &self.0;
+//         let res = Signature::from_bytes(data);
+//         res
+//     }
+//     pub fn from_signature(sign: &Signature) -> Self {
+//         let data = sign.to_bytes();
+//         SignatureInfo(data)
+//     }
+
+//     pub fn from_slice(data: &[u8]) -> Result<Self, SignatureError> {
+//         // let mut sig = [0u8; SIGNATURE_LENGTH];
+//         // sig[..].copy_from_slice(data);
+//         let sig: Signature = Signature::from_bytes(data).unwrap();
+//         Ok(SignatureInfo::from_signature(&sig))
+//     }
+// }
